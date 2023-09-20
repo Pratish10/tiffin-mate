9e1a35861db7de0a11eb655150ce51845fe54360
Pratish Ninawe <pratish.1006@gmail.com>
2023-09-20 10:49:50 +0530

    fixed fonts

--- src/pages/Menu/index.jsx
+++ src/pages/Menu/index.jsx
@@ -5,38 +5,37 @@ import Foods from "components/Foods";
 
 const MenuPage = () => {
   return (
-    // <div className="bg-white-A700 flex flex-col items-center justify-start mx-auto w-full">
-    //   <div className="flex flex-col items-center justify-start w-full">
-    //     <Text
-    //       className="mt-[41px] sm:text-4xl md:text-[42px] text-[46px] text-black-900"
-    //       size="txtMontserratRomanSemiBold46"
-    //     >
-    //       Menu
-    //     </Text>
-    //     <Text
-    //       className="mt-[9px] text-2xl md:text-[22px] text-blue_gray-900 sm:text-xl w-[89%] sm:w-full"
-    //       size="txtMontserratRomanRegular24"
-    //     >
-    //       Lorem ipsum dolor sit amet consectetur. Nascetur felis enim ac sem
-    //       volutpat. Egestas nunc nibh pulvinar mollis volutpat morbi aliquam
-    //       feugiat. Lacus id aliquam volutpat arcu. Pellentesque vestibulum
-    //       ornare eu nulla faucibus.{" "}
-    //     </Text>
-    //     <Foods />
-    //     <Text
-    //       className="mt-[46px] text-2xl md:text-[22px] text-blue_gray-900 sm:text-xl w-[93%] sm:w-full"
-    //       size="txtMontserratRomanRegular24"
-    //     >
-    //       Lorem ipsum dolor sit amet consectetur. Nascetur felis enim ac sem
-    //       volutpat. Egestas nunc nibh pulvinar mollis volutpat morbi aliquam
-    //       feugiat. Lacus id aliquam volutpat arcu. Pellentesque vestibulum
-    //       ornare eu nulla faucibus. Eu bibendum magna in orci et.{" "}
-    //     </Text>
-    //     <br />
-    //     <br />
-    //   </div>
-    // </div>
-    <>Menu</>
+    <div className="bg-white-A700 flex flex-col items-center justify-start mx-auto w-full">
+      <div className="flex flex-col items-center justify-start w-full">
+        <Text
+          className="mt-[41px] sm:text-4xl md:text-[42px] text-[46px] text-black-900"
+          size="txtMontserratRomanSemiBold46"
+        >
+          Menu
+        </Text>
+        <Text
+          className="mt-[9px] text-2xl md:text-[22px] text-blue_gray-900 sm:text-xl w-[89%] sm:w-full"
+          size="txtMontserratRomanRegular24"
+        >
+          Lorem ipsum dolor sit amet consectetur. Nascetur felis enim ac sem
+          volutpat. Egestas nunc nibh pulvinar mollis volutpat morbi aliquam
+          feugiat. Lacus id aliquam volutpat arcu. Pellentesque vestibulum
+          ornare eu nulla faucibus.{" "}
+        </Text>
+        <Foods />
+        <Text
+          className="mt-[46px] text-2xl md:text-[22px] text-blue_gray-900 sm:text-xl w-[93%] sm:w-full"
+          size="txtMontserratRomanRegular24"
+        >
+          Lorem ipsum dolor sit amet consectetur. Nascetur felis enim ac sem
+          volutpat. Egestas nunc nibh pulvinar mollis volutpat morbi aliquam
+          feugiat. Lacus id aliquam volutpat arcu. Pellentesque vestibulum
+          ornare eu nulla faucibus. Eu bibendum magna in orci et.{" "}
+        </Text>
+        <br />
+        <br />
+      </div>
+    </div>
   );
 };
 
